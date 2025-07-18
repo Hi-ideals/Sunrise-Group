ea9c5564f0125a8cc52bbbbd3e4d06891738f65d
Hi-ideals <hiidealstechnologies@gmail.com>
2025-07-18 03:34:56 -0700

    nuring-v1

---
+++ application/config/license.php
@@ -0,0 +1,10 @@
+<?php
+
+if (!defined('BASEPATH')) {
+    exit('No direct script access allowed');
+}
+
+$config['envato_market_purchase_code'] = '';
+$config['envato_market_username'] = '';
+$config['SSLK'] = '7RKFFE-DAUXCJ-QHUZ9M-ZjhXem0vNy9oMG5uc2xyOEUveDNLNU1kTG9mVFJsc3l0Y0swT2pnVUNKL0ZQL2tSLzZhR29HdjV0UUdFeHdqVw==';
+$config['app_ver'] = 0;
