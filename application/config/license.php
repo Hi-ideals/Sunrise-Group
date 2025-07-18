6cd96bf14174bafed92f82c97b5fd839571138b1
Hi-ideals <hiidealstechnologies@gmail.com>
2025-07-18 02:51:16 -0700

    educare-v1

---
+++ application/config/license.php
@@ -0,0 +1,12 @@
+<?php
+
+if (!defined('BASEPATH')) {
+    exit('No direct script access allowed');
+}
+
+$config['envato_market_purchase_code'] = '';
+$config['envato_market_username'] = '';
+$config['SSLK'] = '26YNC2-MG7ZC7-TEK2T7-UjJyOHR6VlN1Ti9lL0xHOVgxNHhLeCtvNWx2VWp6Sm82M25GaDJNRnJLN2lOSjJpK3dqTUlRUHVKS3hldHFCbQ==';
+$config['app_ver'] = 0;
+$config['addon_prod'] = array();
+$config['addon_ver'] = array();
