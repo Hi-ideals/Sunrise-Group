e3f9d346f2ea94ba82e7814952e7f1cd1a4300ed
subhashinihiremath <subhashinihiremath2002@gmail.com>
2025-07-18 11:47:21 +0530

    Initial commit of nursing project

---
+++ application/config/license.php
@@ -0,0 +1,10 @@
+<?php
+
+if (!defined('BASEPATH')) {
+    exit('No direct script access allowed');
+}
+
+$config['envato_market_purchase_code'] = '';
+$config['envato_market_username'] = '';
+$config['SSLK'] = 'WM6CLM-T82YDT-UPLJRC-OHl5cEdXSVFHaytsc3I1NFJRaG4xcWIvcHBUNUFKRXAvbWkydVBOZlRxNWJwL3JDaUJHc3pTbXZJTStQZHZYWHhvUCszY3U5eXJlTjVYMHV0QUhpdmc9PQ==';
+$config['app_ver'] = 0;
