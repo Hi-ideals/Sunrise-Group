5c245f7403da0d1153fa721058b8144e74065d88
Hi-ideals <hiidealstechnologies@gmail.com>
2025-07-21 04:10:41 -0700

    sharnahal-v1

---
+++ application/config/license.php
@@ -0,0 +1,10 @@
+<?php
+
+if (!defined('BASEPATH')) {
+    exit('No direct script access allowed');
+}
+
+$config['envato_market_purchase_code'] = '';
+$config['envato_market_username'] = '';
+$config['SSLK'] = 'L7TE2H-2TGHHN-JRMSKS-N09ldnVEYlR2eVVrM0QzMHZSM3YwNlhONng5SVVqa2VONmRsYjNodE42Q0ZJTXhJQlZlUnZqTEhzcGYxbjVqeXJGTWZ1d2FWcEdyY1AyNUhJM01jM2c9PQ==';
+$config['app_ver'] = 0;
