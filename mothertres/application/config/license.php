914f8fdfc4af05e6cd2c3b2ebc4013770b2fed33
Hi-ideals <hiidealstechnologies@gmail.com>
2025-07-21 04:26:41 -0700

    mothertres-v1

---
+++ mothertres/application/config/license.php
@@ -0,0 +1,10 @@
+<?php
+
+if (!defined('BASEPATH')) {
+    exit('No direct script access allowed');
+}
+
+$config['envato_market_purchase_code'] = '';
+$config['envato_market_username'] = '';
+$config['SSLK'] = 'VG2A73-W4QE9H-E78EWM-ZzM3dlVsMmVwVnArMnhUcTBpcGtRQmZ1UkVzWjQ4ZStXREVCWjNjeXg5bDJzcUVHREpGQjZTTCtlNk93cHlPN2pob2FFc05hQXJnQ21mZ3p2cGJhMVE9PQ==';
+$config['app_ver'] = 0;
