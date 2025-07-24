10a327cde8b2ff3c5eba8e9f6ea3ffbb18c319bc
Hi-ideals <hiidealstechnologies@gmail.com>
2025-07-24 00:41:10 -0700

    louscnursing-v1

---
+++ kouscnursing/application/config/license.php
@@ -0,0 +1,10 @@
+<?php
+
+if (!defined('BASEPATH')) {
+    exit('No direct script access allowed');
+}
+
+$config['envato_market_purchase_code'] = '';
+$config['envato_market_username'] = '';
+$config['SSLK'] = '2NKT8F-BBRSBV-NPZ5XR-b1Q5YlA0Q0lQMEVldjJZS3gzZlRHZFdxMElISkxrVVo1NGdlanR5eUhGRi9YVks5N3lxc2QwQkNlSzROazNpbXNDT1UyK3BOb3dYRmhqTXQrSzRmS0E9PQ==';
+$config['app_ver'] = 0;
---
+++ suiparscience/application/config/license.php
@@ -0,0 +1,10 @@
+<?php
+
+if (!defined('BASEPATH')) {
+    exit('No direct script access allowed');
+}
+
+$config['envato_market_purchase_code'] = '';
+$config['envato_market_username'] = '';
+$config['SSLK'] = 'DD6SYG-QRWG3A-9KK8PY-a0xUU25QamZiMlNuTVVNUDYzeG1EUzBPR3IrQlRYSmVGRGdMOFpLZGcvSXF4NENyOVBlOTRxTXZoQ3hCTlpLdytETEVudnBORXczYTJXRDhYcWgxeWc9PQ==';
+$config['app_ver'] = 0;
