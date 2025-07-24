53b6b794c43d9dc695f4d3c678ae34798e655402
Hi-ideals <hiidealstechnologies@gmail.com>
2025-07-24 04:20:17 -0700

    sunrisecolphar-v1

---
+++ sunrisecolphar/application/config/license.php
@@ -0,0 +1,10 @@
+<?php
+
+if (!defined('BASEPATH')) {
+    exit('No direct script access allowed');
+}
+
+$config['envato_market_purchase_code'] = '';
+$config['envato_market_username'] = '';
+$config['SSLK'] = 'S927XW-6TDP9K-FLUM8S-NmhVeDdIY3FhT0traDJDMnpaV1AyL0huaEI0N0dMaXBJcmFudURrL0R5Ukw1WFRKdGQwa3R3aWEyQ0NGZjdLWm1YeGxKK2ZHSWIrOFVCUVpUUnR0MFE9PQ==';
+$config['app_ver'] = 0;
