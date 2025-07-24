aafe2a7ee1e54e767ac56ffcbd23a7c1e34e15b4
Hi-ideals <hiidealstechnologies@gmail.com>
2025-07-24 01:25:36 -0700

    hprcolnursing-v1

---
+++ hprcolnursing/application/config/license.php
@@ -0,0 +1,10 @@
+<?php
+
+if (!defined('BASEPATH')) {
+    exit('No direct script access allowed');
+}
+
+$config['envato_market_purchase_code'] = '';
+$config['envato_market_username'] = '';
+$config['SSLK'] = 'E4RJKE-AN63W9-56DBPL-WmN4YnhqTGl2SnZDQ3RoMnYveUlEenk5Ylc5TGl1NHN6d2N0Z3ZVRWs3RWY1TGloSHNrSEVIdjRudUtjYStIaCtKQ0xiQW4yeG9tODRzUzZkYW9mdGc9PQ==';
+$config['app_ver'] = 0;
